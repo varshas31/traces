aaefe140a6fc42326c1c70d5635bd57e5af49aa7
varshas31 <vashasreedhara@gmail.com>
2024-08-05 12:02:49 +0530

    Update the functionality

--- test.py
+++ test.py
@@ -1,27 +1,47 @@
 from trace_module import SimpleTracer
 
 def view_product(product_name):
+    # Trace this function
+    tracer.log("view-product", _view_product_internal, product_name)
+
+def _view_product_internal(product_name):
     print("Viewing product {}".format(product_name))
     # Simulate some work
     for i in range(2):
         print("Loading product details {}".format(i))
+    # Call add_to_cart here
+    add_to_cart(product_name)
 
 def add_to_cart(product_name):
+    # Trace this function
+    tracer.log("add-to-cart", _add_to_cart_internal, product_name)
+
+def _add_to_cart_internal(product_name):
     print("Adding product {} to cart".format(product_name))
     # Simulate some work
     print("Updating cart with product {}".format(1))
+    # Call checkout here
+    checkout("User Cart")
 
 def checkout(cart_name):
+    # Trace this function
+    tracer.log("checkout", _checkout_internal, cart_name)
+
+def _checkout_internal(cart_name):
     print("Checking out {}".format(cart_name))
     # Simulate some work
     print("Processing checkout step {}".format(1))
 
 def main():
-    tracer = SimpleTracer("traces-app")
-    tracer.log("view-product", view_product, "SmartPhone")
-    tracer.log("add-to-cart", add_to_cart, "SmartPhone")
-    tracer.log("checkout", checkout, "User Cart")
+    global tracer
+    tracer = SimpleTracer("example", jaeger_host="localhost", jaeger_port=6831)
+
+    # Start tracing the main function
+    tracer.log("main", _main_internal)
+
+def _main_internal():
+    # Start the trace by calling view_product
+    view_product("SmartPhone")
 
 if __name__ == "__main__":
     main()
-
--- trace_module.py
+++ trace_module.py
@@ -3,16 +3,32 @@ from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExporter
 from opentelemetry.trace import Status, StatusCode
+from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+
 
 class SimpleTracer:
-    def __init__(self, service_name):
+    def __init__(self, service_name, jaeger_host='localhost', jaeger_port=6831):
         resource = Resource(attributes={
             SERVICE_NAME: service_name
         })
         provider = TracerProvider(resource=resource)
+
+        # Set the tracer provider
         trace.set_tracer_provider(provider)
-        span_processor = BatchSpanProcessor(ConsoleSpanExporter())
-        trace.get_tracer_provider().add_span_processor(span_processor)
+
+        # Set up the Jaeger exporter
+        jaeger_exporter = JaegerExporter(
+            agent_host_name=jaeger_host,
+            agent_port=jaeger_port,
+        )
+        span_processor = BatchSpanProcessor(jaeger_exporter)
+        provider.add_span_processor(span_processor)
+
+        # Also log to console
+        console_exporter = ConsoleSpanExporter()
+        console_span_processor = BatchSpanProcessor(console_exporter)
+        provider.add_span_processor(console_span_processor)
+
         self.tracer = trace.get_tracer(service_name)
 
     def log(self, span_name, func, *args, **kwargs):
@@ -26,5 +42,5 @@ class SimpleTracer:
                 raise e
 
 # Usage example:
-# tracer = SimpleTracer("example-service")
+# tracer = SimpleTracer("example-service", jaeger_host="localhost", jaeger_port=6831)
 # tracer.log("span-name", some_function, arg1, arg2, kwarg1=value1)
